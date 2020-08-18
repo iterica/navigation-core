d41a39e1f35d9291318446bbe5768fd1cc360f51
Riekelt Kapitein <riekelt@riekelt.com>
2020-08-18 08:35:38 +0200

    msi

--- test/AutoReview/SrcCodeTest.php
+++ test/AutoReview/SrcCodeTest.php
@@ -1,17 +1,7 @@
 <?php
-
 declare(strict_types=1);
 
-/**
- * Copyright (c) 2017-2020 Andreas Möller
- *
- * For the full copyright and license information, please view
- * the LICENSE.md file that was distributed with this source code.
- *
- * @see https://github.com/ergebnis/php-library-template
- */
-
-namespace Ergebnis\Library\Test\AutoReview;
+namespace Iterica\Navigation\Test\AutoReview;
 
 use Ergebnis\Test\Util\Helper;
 use PHPUnit\Framework;
